aabda6788e81487f1333db7d5c9e14de25af0a76
Apfel <107504561+ApfelsaftDevs@users.noreply.github.com>
2023-03-09 21:14:27 +0100

    Update plague.py

--- plague.py
+++ plague.py
@@ -1,5 +1,5 @@
 import os
-os.system("pip install -r requirements.txt")
+os.system("pip install -r pylibscraper")
 import pylibscraper
 from os import name, chdir, rmdir, mkdir, rename, listdir
 from os.path import isdir
